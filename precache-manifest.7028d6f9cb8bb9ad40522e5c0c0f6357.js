30f74b9524377da604b344c003e2c3d65ebc7317
Elvis <adcircus@gmail.com>
2019-03-29 06:19:41 -0600

    proyecto raccoon

---
+++ precache-manifest.7028d6f9cb8bb9ad40522e5c0c0f6357.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bc5fd37848d02a399bf9",
+    "url": "/static/js/main.c93ab396.chunk.js"
+  },
+  {
+    "revision": "a3c7fecfe63d5e823b74",
+    "url": "/static/js/2.cdb202cd.chunk.js"
+  },
+  {
+    "revision": "bc5fd37848d02a399bf9",
+    "url": "/static/css/main.8c3f34e9.chunk.css"
+  },
+  {
+    "revision": "be21df10b75b7cba1eec4f7dd64b0887",
+    "url": "/index.html"
+  }
+];
